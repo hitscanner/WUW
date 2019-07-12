8ace26d7b38b4ce1c013c6d287795913114521b1
dmswl0311 <dmswl_0311@naver.com>
2019-07-12 19:54:23 +0900

    3

--- WUW/urls.py
+++ WUW/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import app.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',app.views.index,name='index'),
 ]
