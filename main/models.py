09ed9eeb97fbf0a820e1ec5438b0a11cb370e44f
hitscanner <rlaehdcuf@gmail.com>
2019-07-16 18:20:10 +0900

    model 임시완료

--- main/models.py
+++ main/models.py
@@ -4,12 +4,13 @@ from django.db import models
 class Search_result(models.Model):
     title=models.CharField(max_length=100)
     poster=models.ImageField(blank=True)
-    heart=models.ImageField(blank=True)
+    like=models.IntegerField(blank=True)
+    actor=models.CharField(max_length=100)
+    rating=models.CharField(max_length=100)
+    OST=models.CharField(max_length=100)
     created_at=models.DateField(auto_now_add= True)
     updated_at = models.DateField(auto_now = True)
 
+class calendar(models.Model):
 
-
-
-
-    
+class joahyo(models.Model):
