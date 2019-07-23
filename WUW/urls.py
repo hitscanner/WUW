51347a9493ea7914c326b314a3ad0518b49d1e0b
hitscanner <rlaehdcuf@gmail.com>
2019-07-24 04:03:32 +0900

    fullcalendar

--- WUW/urls.py
+++ WUW/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path
 import main.views
 import accounts.views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
