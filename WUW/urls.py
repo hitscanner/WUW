aecde55ccd183cb9fce828ff7a5483c428d3a9bf
ha0617 <a36206522@gmail.com>
2019-08-02 17:34:46 +0900

    Ranking

--- WUW/urls.py
+++ WUW/urls.py
@@ -29,5 +29,6 @@ urlpatterns = [
     path('logout/', accounts.views.logout, name='logout'),
     path('search/',main.views.search,name='search'),
     path('shoppingbag/', main.views.shoppingbag, name='shoppingbag'),
+    path('ranking/', main.views.ranking, name='ranking'),
     path('fullcalendar/',fullcalendar.views.fullcalendar,name='fullcalendar'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+import requests
+from bs4 import BeautifulSoup
 
 # Create your views here.
 def index(request):
+    
 
     return render(request,'index.html')
 
@@ -12,4 +15,23 @@ def search(request):
 def shoppingbag(request):
 
     return render(request, 'shoppingbag.html')
+
+def ranking(request):
+    url1 = "https://movie.naver.com/movie/sdb/rank/rmovie.nhn"
+
+    html_ranking1 = requests.get(url1).text
+    soup_ranking1 = BeautifulSoup(html_ranking1, "lxml")
+
+    rankings1 = soup_ranking1.select('div.tit3')
+    Movie_ranking1 = [ranking1.get_text().strip() for ranking1 in rankings1]
+
+    
+    url2 = "https://movie.naver.com/movie/sdb/rank/rmovie.nhn?sel=pnt&date=20190801"
+    html_ranking2 = requests.get(url2).text
+    soup_ranking2 = BeautifulSoup(html_ranking2, "lxml")
+
+    rankings2 = soup_ranking2.select('div.tit5')
+    Movie_ranking2 = [ranking2.get_text().strip() for ranking2 in rankings2]
+
+    return render(request, 'ranking.html',{"Movie_ranking1":Movie_ranking1, "Movie_ranking2":Movie_ranking2})
 
