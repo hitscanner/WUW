d2bc316391081e2e4723429052a3c97fbdb6fc07
dmswl0311 <dmswl_0311@likelion.org>
2019-08-02 17:48:51 +0900

    model

--- main/models.py
+++ main/models.py
@@ -2,12 +2,23 @@ from django.db import models
 
 # Create y models here.
 class Search_result(models.Model):
-    title=models.CharField(max_length=100)
-    poster=models.ImageField(blank=True)
+    name=models.CharField(max_length=100)
+    poster=models.TextField(blank=True)
     # like=models.ManyToManyField(User)
     youtube = models.BooleanField(default=False) 
     netflix = models.BooleanField(default=False)
     watcha = models.BooleanField(default=False)
+
+    action=models.BooleanField(default=False)
+    fantasy=models.BooleanField(default=False)
+    thriller=models.BooleanField(default=False)
+    comedy=models.BooleanField(default=False)
+    romance=models.BooleanField(default=False)
+    drama=models.BooleanField(default=False)
+    sf=models.BooleanField(default=False)
+    horror=models.BooleanField(default=False)
+    animation=models.BooleanField(default=False)
+
     created_at=models.DateField(auto_now_add= True)
     updated_at = models.DateField(auto_now = True)
 
