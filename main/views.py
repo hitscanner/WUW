45a687eb1cb5ea293bb4fde4c69d3f47b0349ad9
unknown <bhj1684@ynu.ac.kr>
2019-08-03 20:03:17 +0900

    detail_url

--- main/views.py
+++ main/views.py
@@ -63,25 +63,3 @@ def ranking(request):
 
     return render(request, 'ranking.html',{"Movie_ranking1":Movie_ranking1, "Movie_ranking2":Movie_ranking2})
 
-def export_csv(modeladmin, request, queryset):
-    import csv
-    from django.utils.encoding import smart_str
-    response = HttpResponse(content_type='text/csv')
-    response['Content-Disposition'] = 'attachment; filename=watcha_TF.csv'
-    writer = csv.writer(response, csv.excel)
-    response.write(u'\ufeff'.encode('utf8')) # BOM (optional...Excel needs it to open UTF-8 file properly)
-    writer.writerow([
-        smart_str(u"ID"),
-        smart_str(u"Name"),
-        smart_str(u"Netflix"),
-        smart_str(u"Watcha"),
-    ])
-    for obj in queryset:
-        writer.writerow([
-            smart_str(obj.pk),
-            smart_str(obj.name),
-            smart_str(obj.netflix),
-            smart_str(obj.watcha),
-        ])
-    return response
-export_csv.short_description = u"Export CSV"
