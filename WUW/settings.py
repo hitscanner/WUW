cc9935b1e56bdd49c661002c37b1dc8bec5dab0f
hitscanner <rlaehdcuf@gmail.com>
2019-08-09 16:59:59 +0900

    dfdfef

--- WUW/settings.py
+++ WUW/settings.py
@@ -123,14 +123,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 
 STATICFILES_DIRS = [
     # os.path.join(BASE_DIR, 'static'),
     os.path.join(BASE_DIR,'main' ,'static'),
     os.path.join(BASE_DIR,'fullcalendar' ,'static'),
 ] 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Heroku: Update database configuration from $DATABASE_URL.
 import dj_database_url
